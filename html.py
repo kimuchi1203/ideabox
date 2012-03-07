723be6b79b2a4c92f4e91db8df92686e6824ab53
Kimura Mitsuhiro <kimura1203@gmail.com>
2012-03-07 22:31:27 +0900

    use template

---
+++ html.py
@@ -0,0 +1,34 @@
+import os
+import cgi
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        if users.get_current_user():
+            login_url = users.create_logout_url(self.request.uri)
+            login_url_linktext = 'Logout'
+        else:
+            login_url = users.create_login_url(self.request.uri)
+            login_url_linktext = 'Login'
+
+        template_values = {
+            'login_url': login_url,
+            'login_url_linktext': login_url_linktext,
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication([('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
+
